c99f0f23106140d26ce943cde332f1c5aea71e75
AppiahEnoch <prignutt@gmail.com>
2022-12-01 02:17:04 -1200

    aaaaa

---
+++ addNewFamilyMember.php
@@ -0,0 +1,136 @@
+<?php
+require_once 'config.php';
+
+
+
+
+$v1="name";
+
+
+
+
+// declare all fields
+$newName= cleanInput( $_POST[$v1]);
+
+
+
+
+
+
+
+// array to test post and set status of vital variables
+$arrayOfAllevels=[$v1];
+
+// function to clean user input
+function cleanInput($data){
+    try {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+      } catch(Exception $e) {
+
+      
+        
+      }
+     return $data;
+}
+
+
+
+
+
+// functin to test issetand post variables
+function inputsAreCorrect( $arrayOfAllevels) {
+    $r=sizeof($arrayOfAllevels)-1;
+
+    
+    for ($i = 0; $i <= $r; $i++) {
+        $fieldName=$arrayOfAllevels[$i];
+     
+        try{
+            
+            
+        if (isset($_POST[$fieldName])) {
+
+            if (empty($_POST[$fieldName])) {
+
+               
+              
+                return false;
+                
+            }
+            
+        }
+        else{
+            
+        
+            return false;
+        } 
+        } 
+        catch(Exception $e){
+
+            
+        }
+       
+        
+      
+      }
+    
+    return true;
+  }
+
+
+  // check to insert data if everything is fine.
+  if(!inputsAreCorrect($arrayOfAllevels)){
+    exit();
+  }
+
+
+
+// prepare and bind
+try{
+    
+    $sql = "SELECT * FROM familyMember where mName='$newName'";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+      // output data of each row
+      if($row = mysqli_fetch_assoc($result)) {
+    
+        echo "1";
+    
+      }
+    
+    
+    
+    }
+     else {
+    
+        echo 0;
+     
+        }
+
+ 
+
+
+    
+}
+catch(Exception $e){
+    echo $e;
+    
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+
+  
+  
---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "funeralDonation";
+//$port = "5555";
+//$conn = mysqli_connect($hostname, $username, $password, $database,$port) or die("Database connection failed");
+
+/*
+// heroku
+$hostname = "us-cdbr-east-06.cleardb.net";
+$username = "b8052e1675b349";
+$password = "226c3dab";
+$database = "heroku_d0301c5e9ed1a1c";
+//*/
+//*
+
+// railway
+
+/*
+
+$hostname = "containers-us-west-98.railway.app";
+$username = "root";
+$password = "ismm28lwOe2ySW0pZhpq";
+$database = "railway";
+$port = "6385";
+$conn = mysqli_connect($hostname, $username, $password, $database,$port) or die("Database connection failed");
+
+//*/
+?>
