70cd27127b12f48c177141e676e276acbc214c83
AppiahEnoch <prignutt@gmail.com>
2022-12-06 22:27:13 -1200

    eeeeee

---
+++ insert_expense.php
@@ -0,0 +1,111 @@
+<?php
+require_once 'config.php';
+
+
+
+
+$v1="amount";
+$v2="description";
+
+
+
+// declare all fields
+$amount= cleanInput( $_POST[$v1]);
+$description= cleanInput(   $_POST[$v2]);
+
+
+
+
+// array to test post and set status of vital variables
+$arrayOfAllNames=[$v1,$v2];
+
+// function to clean user input
+function cleanInput($data){
+    try {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+      } catch(Exception $e) {
+
+      
+        
+      }
+     return $data;
+}
+
+
+
+
+
+// functin to test issetand post variables
+function inputsAreCorrect( $arrayOfAllNames) {
+    $r=sizeof($arrayOfAllNames)-1;
+
+    
+    for ($i = 0; $i <= $r; $i++) {
+        $fieldName=$arrayOfAllNames[$i];
+     
+        try{
+            
+            
+        if (isset($_POST[$fieldName])) {
+
+            if (empty($_POST[$fieldName])) {
+
+                echo  $fieldName;
+              
+                return false;
+                
+            }
+            
+        }
+        else{
+            
+        
+            return false;
+        } 
+        } 
+        catch(Exception $e){
+
+            
+        }
+       
+        
+      
+      }
+    
+    return true;
+  }
+
+
+  // check to insert data if everything is fine.
+  if(!inputsAreCorrect($arrayOfAllNames)){
+    exit();
+  }
+
+
+
+  
+// prepare and bind
+try{
+
+$stmt = $conn->prepare("INSERT INTO expense
+ (amount, `description`) VALUES (?, ?)");
+$stmt->bind_param("ss", $amount, $description);
+ $stmt->execute();
+
+ echo 1;
+
+$stmt->close();
+$conn->close();
+    
+}
+catch(Exception $e){
+
+    
+}
+
+
+
+  
+  
